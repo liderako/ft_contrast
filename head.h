7718e18a403907b9fd85df0ca2c5f84a9a3c68dd
Anton Sviridov <asvirido@e2r6p9.unit.ua>
2018-02-03 11:06:39 +0200

    modified main

--- head.h
+++ head.h
@@ -6,12 +6,22 @@
 /*   By: asvirido <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/12 18:00:34 by asvirido          #+#    #+#             */
-/*   Updated: 2017/05/17 22:49:38 by asvirido         ###   ########.fr       */
+/*   Updated: 2018/02/03 11:03:51 by asvirido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEAD_H
 # define HEAD_H
 # include "src/general/general.h"
+
+typedef struct		s_contrast
+{
+	int			*mapping_pixel;
+	int			lenght;
+	int			height;
+	int			max_grey_lvl;
+	int			contrast;
+	char		*name_new_file;
+}					t_contrast;
 
 #endif
--- main.c
+++ main.c
@@ -12,22 +12,31 @@
 
 #include "head.h"
 
+// t_contrast	*create_mapping(t_lst *list)
+// {
+// 	t_contrast		*main;
+
+// 	main = (t_contrast*)malloc(sizeof(t_contrast));
+// 	return (main);
+// }
+
 int		main(int argc, char **argv)
 {
-	t_lst	*list_read;
+	t_lst			*list_read;
+	// t_contrast		*main;
 
-	if (input_param(argc, argv) == 1)
+	if (input_param(argc, argv) != 1)
+		return (-1);
+	list_read = ft_read(argv);
+	if (list_read->len == 0)
 	{
-		list_read = ft_read(argv);
-		if (list_read->len == 0)
-		{
-			printf("Error file\n");
-			return (0);
-		}
-		while (list_read->next)
-		{
-			printf("%s\n", list_read->line);
-			list_read = list_read->next;
-		}
+		printf("Error file\n");
+		return (0);
 	}
+	//main = create_mapping(list_read);
+	// while (list_read->next)
+	// {
+	// 	printf("%s\n", list_read->line);
+	// 	list_read = list_read->next;
+	// }
 }
